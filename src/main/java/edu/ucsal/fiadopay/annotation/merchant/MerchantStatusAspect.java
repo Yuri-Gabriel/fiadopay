29e7e7d5441da2d6b3be27a028450997ba8a9b2a
Isabel Alves <isabel_alves02@hotmail.com>
2025-11-20 13:26:25 -0300

    Add AOP + Annotation + Reflexion para classe Merchant

---
+++ src/main/java/edu/ucsal/fiadopay/annotation/merchant/MerchantStatusAspect.java
@@ -0,0 +1,45 @@
+package edu.ucsal.fiadopay.annotation.merchant;
+
+import java.lang.reflect.Field;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import edu.ucsal.fiadopay.annotation.logs.Logger;
+
+@Aspect
+@Component
+public class MerchantStatusAspect {
+  @Logger
+  @Around("@annotation(MerchantStatus)")
+  public Object around(ProceedingJoinPoint joinPoint, MerchantStatus merchantStatus) throws Throwable {
+
+    Object[] args = joinPoint.getArgs();
+    for (Object arg : args) {
+      validate(arg, merchantStatus);
+    }
+
+    return joinPoint.proceed();
+  }
+
+  private void validate(Object target, MerchantStatus annotation) {
+    // reflection pra validar se o atributo status é igual ao que eu espero na
+    // anotação
+    try {
+      Field field = target.getClass().getDeclaredField("status");
+      field.setAccessible(true);
+      Object statusValue = field.get(target);
+
+      if (!statusValue.equals(annotation.value())) {
+        throw new IllegalStateException(
+            String.format("Status inválido: esperado %s, recebido %s",
+                annotation.value(), statusValue));
+      }
+    } catch (Exception e) {
+      throw new RuntimeException("O comerciante não possui o campo 'Status'.");
+    }
+  }
+
+}
